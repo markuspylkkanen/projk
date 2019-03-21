53072c45ebbdb226043ad1e6f1ea92b886ee3807
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2019-03-21 10:02:09 +0200

    on

--- viestiseinä/insert.php
+++ viestiseinä/insert.php
@@ -4,9 +4,9 @@ $username = "root";
 $password = "password";
 $dbname = "";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
