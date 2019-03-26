f2ab657b7d3fdb1a6234a11a2ded7c20cb858ba2
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2019-03-26 11:30:45 +0200

    26.3

--- viestiseinä/insert.php
+++ viestiseinä/insert.php
@@ -1,9 +1,24 @@
 <?php
+
 echo date("l jS \of F Y h:i:s A") . "<br>";
+
+// Kirjoitetaan data tekstitiedostoon
+if (filter_has_var(INPUT_POST, 'submit')){
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    
+    $myfile = fopen("tiedot.txt", "a") or die("");
+    $rivi = "$name;$message\n";
+    fwrite($myfile, $rivi);
+    fclose($myfile);
+}
+
+// Tallennetaan tiedot kantaan
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "";
 $dbname = "";
+
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
