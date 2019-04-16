46eac36dec016e34ff8be88bd30f0b2e818db15b
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2019-04-16 12:37:12 +0300

    select

---
+++ viestiseinä/select.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "qwerty";
+$dbname = "seina";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT viestit, viesti, pvm, nimi FROM seina";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
