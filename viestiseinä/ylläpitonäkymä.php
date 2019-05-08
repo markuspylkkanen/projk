0b3d449b9ea5b59b40768cca6eabf1a7d5330035
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2019-05-08 09:56:22 +0300

    admin update

--- viestiseinä/ylläpitonäkymä.php
+++ viestiseinä/ylläpitonäkymä.php
@@ -2,3 +2,20 @@
 
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet">   
+<link rel="stylesheet" type="text/css" href="ylläpitonäkymä.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<title>Ylläpitonäkymä</title>
+<div class="b">
+<i><h1>Ylläpitonäkymä</h1></i>
+</head>
+<body>
+</div>    
+</body>
+</html>
