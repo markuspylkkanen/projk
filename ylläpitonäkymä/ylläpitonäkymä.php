9a575dc330357ff29bffde9c436457357ef80378
markus pylkkänen <markus.pylkkanen@itvelhot.local>
2019-05-22 10:57:22 +0300

    käyttöliittymä

--- ylläpitonäkymä/ylläpitonäkymä.php
+++ ylläpitonäkymä/ylläpitonäkymä.php
@@ -8,20 +8,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet">   
-<link rel="stylesheet" type="text/css" href="ylläpitonäkymä.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<title>Ylläpitonäkymä</title>
-<div class="b">
-<i><h1>Ylläpitonäkymä</h1></i>
+    <link href="https://fonts.googleapis.com/css?family=Patua+One" rel="stylesheet">   
+    <link rel="stylesheet" type="text/css" href="ylläpitonäkymä.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Ylläpitonäkymä</title>
+    <style>
+    .viesti {
+        margin-top: 15px;
+    }
+    </style>
+
+
 </head>
 <body>
-<br>
-<button onclick="">Piilota</button>
-<button onclick="">Poista</button>
-<button onclick="">Näytä</button>
+<h1>Ylläpitonäkymä</h1>
 
+<div class="container">
+    <div class="row">
+        <div class="col">      
+            
+            <div class="card viesti">
+                <div class="card-body">
+                    <h5 class="card-title">Lähettäjä</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Piilota</a>
+                    <a href="#" class="btn btn-danger">Poista</a>
+                </div>
+            </div>
 
+            <div class="card viesti">
+                <div class="card-body">
+                    <h5 class="card-title">Lähettäjä</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Piilota</a>
+                    <a href="#" class="btn btn-danger">Poista</a>
+                </div>
+            </div>
+
+            <div class="card viesti">
+                <div class="card-body">
+                    <h5 class="card-title">Lähettäjä</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Piilota</a>
+                    <a href="#" class="btn btn-danger">Poista</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>    
 </body>
 </html>
