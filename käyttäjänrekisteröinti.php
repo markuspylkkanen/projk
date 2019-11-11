fb4164eb06c308edb1095d1e9cb1bd65587010c7
Markus <36534781+markuspylkkanen@users.noreply.github.com>
2019-11-11 10:58:45 +0200

    Add files via upload

---
+++ käyttäjänrekisteröinti.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Rekisteröinti</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+<br>
+<form class="form-inline" action="/.php">
+  <label for="email">Sähköposti:</label>
+  <input type="email" class="form-control" id="email">
+  
+  <label for="pwd">Salasana:</label>
+  <input type="password" class="form-control" id="pwd">
+  <div class="form-check">
+    
+    </div>
+  <button type="submit" class="btn btn-primary">Luo tili</button>
+</form>
+</form>
+</body>
+</html>
